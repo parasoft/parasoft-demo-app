30b147ae3717960aa2d3eaa8038f0a1bf40c9f48
Roller Wang <rwang@parasoft.com>
2022-09-07 09:03:34 +0800

    Reindent files with 4 spaces.

--- src/main/java/com/parasoft/demoapp/controller/OrderController.java
+++ src/main/java/com/parasoft/demoapp/controller/OrderController.java
@@ -34,150 +34,150 @@ import java.util.List;
 @RequestMapping(value = {"/v1/orders", "/proxy/v1/orders"}, produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderController {
 
-	@Autowired
-	private OrderService orderService;
+    @Autowired
+    private OrderService orderService;
 
-	@Autowired
-	private OrderMQService orderMQService;
+    @Autowired
+    private OrderMQService orderMQService;
 
-	@Autowired
-	private DemoBugService demoBugService;
+    @Autowired
+    private DemoBugService demoBugService;
 
-	@Operation(description = "Submit a new order.")
-	@ApiResponse(responseCode = "200", description = "New order submitted successfully.")
-	@ApiResponse(responseCode = "400", description = "Invalid request payload.",
-				content = {@Content(schema = @Schema(hidden = true))})
-	@ApiResponse(responseCode = "401", description = "You are not authorized to submit a new order.",
-			content = {@Content(schema = @Schema(hidden = true)) })
-	@ApiResponse(responseCode = "403", description = "You do not have permission to submit a new order.",
-			content = {@Content(schema = @Schema(hidden = true)) })
-	@ApiResponse(responseCode = "404", description = "There is no cart item or item to create a new order.",
-			content = {@Content(schema = @Schema(hidden = true))})
-	@PostMapping
-	@ResponseBody
-	public ResponseResult<OrderEntity> addNewOrder(Authentication auth,  @RequestBody OrderDTO orderDto )
-			throws ParameterException, ItemNotFoundException, CartItemNotFoundException {
+    @Operation(description = "Submit a new order.")
+    @ApiResponse(responseCode = "200", description = "New order submitted successfully.")
+    @ApiResponse(responseCode = "400", description = "Invalid request payload.",
+            content = {@Content(schema = @Schema(hidden = true))})
+    @ApiResponse(responseCode = "401", description = "You are not authorized to submit a new order.",
+            content = {@Content(schema = @Schema(hidden = true)) })
+    @ApiResponse(responseCode = "403", description = "You do not have permission to submit a new order.",
+            content = {@Content(schema = @Schema(hidden = true)) })
+    @ApiResponse(responseCode = "404", description = "There is no cart item or item to create a new order.",
+            content = {@Content(schema = @Schema(hidden = true))})
+    @PostMapping
+    @ResponseBody
+    public ResponseResult<OrderEntity> addNewOrder(Authentication auth,  @RequestBody OrderDTO orderDto )
+            throws ParameterException, ItemNotFoundException, CartItemNotFoundException {
 
-		ResponseResult<OrderEntity> response = ResponseResult.getInstance(
-				ResponseResult.STATUS_OK, ResponseResult.MESSAGE_OK);
+        ResponseResult<OrderEntity> response = ResponseResult.getInstance(
+                ResponseResult.STATUS_OK, ResponseResult.MESSAGE_OK);
 
-		Long currentUserId = AuthenticationUtil.getUserIdInAuthentication(auth);
-		String currentUserName = AuthenticationUtil.getUsernameInAuthentication(auth);
-		OrderEntity order = orderService.addNewOrderSynchronized(currentUserId, currentUserName, orderDto.getRegion(), orderDto.getLocation(),
-				orderDto.getReceiverId(), orderDto.getEventId(), orderDto.getEventNumber());
-		response.setData(order);
+        Long currentUserId = AuthenticationUtil.getUserIdInAuthentication(auth);
+        String currentUserName = AuthenticationUtil.getUsernameInAuthentication(auth);
+        OrderEntity order = orderService.addNewOrderSynchronized(currentUserId, currentUserName, orderDto.getRegion(), orderDto.getLocation(),
+                orderDto.getReceiverId(), orderDto.getEventId(), orderDto.getEventNumber());
+        response.setData(order);
 
-		OrderMQMessageDTO message =
-				new OrderMQMessageDTO(order.getOrderNumber(), order.getStatus(), OrderMessages.ORDER_STATUS_CHANGED);
-		orderMQService.sendToApprover(message);
+        OrderMQMessageDTO message =
+                new OrderMQMessageDTO(order.getOrderNumber(), order.getStatus(), OrderMessages.ORDER_STATUS_CHANGED);
+        orderMQService.sendToApprover(message);
 
-		return response;
-	}
+        return response;
+    }
 
-	@Operation(description = "Obtain an order by order number.")
-	@ApiResponse(responseCode = "200", description = "Order with corresponding order number was returned.")
-	@ApiResponse(responseCode = "400", description = "Invalid request parameter.",
-				content = {@Content(schema = @Schema(hidden = true))})
-	@ApiResponse(responseCode = "401",description = "You are not authorized to get an order by order number.",
-				content = {@Content(schema = @Schema(hidden = true)) })
-	@ApiResponse(responseCode = "404", description = "No order with corresponding order number.",
-				content = {@Content(schema = @Schema(hidden = true))})
-	@GetMapping("/{orderNumber}")
-	@ResponseBody
-	public ResponseResult<OrderEntity> getOrderByOrderNumber(@PathVariable String orderNumber)
-			throws ParameterException, OrderNotFoundException, GlobalPreferencesMoreThanOneException,
-				GlobalPreferencesNotFoundException, DemoBugsIntroduceFailedException {
+    @Operation(description = "Obtain an order by order number.")
+    @ApiResponse(responseCode = "200", description = "Order with corresponding order number was returned.")
+    @ApiResponse(responseCode = "400", description = "Invalid request parameter.",
+            content = {@Content(schema = @Schema(hidden = true))})
+    @ApiResponse(responseCode = "401",description = "You are not authorized to get an order by order number.",
+            content = {@Content(schema = @Schema(hidden = true)) })
+    @ApiResponse(responseCode = "404", description = "No order with corresponding order number.",
+            content = {@Content(schema = @Schema(hidden = true))})
+    @GetMapping("/{orderNumber}")
+    @ResponseBody
+    public ResponseResult<OrderEntity> getOrderByOrderNumber(@PathVariable String orderNumber)
+            throws ParameterException, OrderNotFoundException, GlobalPreferencesMoreThanOneException,
+            GlobalPreferencesNotFoundException, DemoBugsIntroduceFailedException {
 
-		ResponseResult<OrderEntity> response = ResponseResult.getInstance(
-				ResponseResult.STATUS_OK, ResponseResult.MESSAGE_OK);
+        ResponseResult<OrderEntity> response = ResponseResult.getInstance(
+                ResponseResult.STATUS_OK, ResponseResult.MESSAGE_OK);
 
-		OrderEntity order = orderService.getOrderByOrderNumber(orderNumber);
-		demoBugService.introduceBugWithIncorrectLocationForApprovedOrdersIfNeeded(order);
+        OrderEntity order = orderService.getOrderByOrderNumber(orderNumber);
+        demoBugService.introduceBugWithIncorrectLocationForApprovedOrdersIfNeeded(order);
 
-		response.setData(order);
+        response.setData(order);
 
-		return response;
-	}
+        return response;
+    }
 
-	@Operation(description = "Update status of order by order number.")
-	@ApiResponse(responseCode = "200", description = "Modified order with corresponding order number was returned.")
-	@ApiResponse(responseCode = "400", description = "Invalid request parameter.",
-			content = {@Content(schema = @Schema(hidden = true))})
-	@ApiResponse(responseCode = "401",description = "You are not authorized to change status.",
-			content = {@Content(schema = @Schema(hidden = true))})
-	@ApiResponse(responseCode = "403",description = "You do not have permission to change status.",
-			content = {@Content(schema = @Schema(hidden = true))})
-	@ApiResponse(responseCode = "404", description = "No order with corresponding order number.",
-			content = {@Content(schema = @Schema(hidden = true))})
-	@PutMapping("/{orderNumber}")
-	@ResponseBody
-	public ResponseResult<OrderEntity> updateStatusOfOrderByOrderNumber(Authentication auth,
-			@RequestBody OrderStatusDTO newStatus, @PathVariable String orderNumber)
-			throws ParameterException, OrderNotFoundException, NoPermissionException, IncorrectOperationException {
+    @Operation(description = "Update status of order by order number.")
+    @ApiResponse(responseCode = "200", description = "Modified order with corresponding order number was returned.")
+    @ApiResponse(responseCode = "400", description = "Invalid request parameter.",
+            content = {@Content(schema = @Schema(hidden = true))})
+    @ApiResponse(responseCode = "401",description = "You are not authorized to change status.",
+            content = {@Content(schema = @Schema(hidden = true))})
+    @ApiResponse(responseCode = "403",description = "You do not have permission to change status.",
+            content = {@Content(schema = @Schema(hidden = true))})
+    @ApiResponse(responseCode = "404", description = "No order with corresponding order number.",
+            content = {@Content(schema = @Schema(hidden = true))})
+    @PutMapping("/{orderNumber}")
+    @ResponseBody
+    public ResponseResult<OrderEntity> updateStatusOfOrderByOrderNumber(Authentication auth,
+                                                                        @RequestBody OrderStatusDTO newStatus, @PathVariable String orderNumber)
+            throws ParameterException, OrderNotFoundException, NoPermissionException, IncorrectOperationException {
 
-		ResponseResult<OrderEntity> response = ResponseResult.getInstance(
-				ResponseResult.STATUS_OK, ResponseResult.MESSAGE_OK);
+        ResponseResult<OrderEntity> response = ResponseResult.getInstance(
+                ResponseResult.STATUS_OK, ResponseResult.MESSAGE_OK);
 
-		response.setData(orderService.updateOrderByOrderNumberSynchronized(
-				orderNumber, AuthenticationUtil.getUserRoleNameInAuthentication(auth), newStatus.getStatus(),
-				newStatus.isReviewedByPRCH(), newStatus.isReviewedByAPV(), AuthenticationUtil.getUsernameInAuthentication(auth),
-				newStatus.getComments(), true));
+        response.setData(orderService.updateOrderByOrderNumberSynchronized(
+                orderNumber, AuthenticationUtil.getUserRoleNameInAuthentication(auth), newStatus.getStatus(),
+                newStatus.isReviewedByPRCH(), newStatus.isReviewedByAPV(), AuthenticationUtil.getUsernameInAuthentication(auth),
+                newStatus.getComments(), true));
 
-		return response;
-	}
+        return response;
+    }
 
-	//	@Operation(description = "Obtain all orders under current identity.")
-	//	@ApiResponses({
-	//		@ApiResponse(responseCode = "200",
-	//					description = "All orders under current identity got successfully."),
-	//		@ApiResponse(responseCode = "403",
-	//					description = "You do not have permission to get all orders.",
-	//					content = {@Content(schema = @Schema(hidden = true)) }),
-	//	})
-	//	@GetMapping("/v1/orders")
-	//	@ResponseBody
-	@Deprecated
-	public ResponseResult<List<OrderEntity>> showAllOrders(Authentication auth) throws ParameterException {
+    //	@Operation(description = "Obtain all orders under current identity.")
+    //	@ApiResponses({
+    //		@ApiResponse(responseCode = "200",
+    //					description = "All orders under current identity got successfully."),
+    //		@ApiResponse(responseCode = "403",
+    //					description = "You do not have permission to get all orders.",
+    //					content = {@Content(schema = @Schema(hidden = true)) }),
+    //	})
+    //	@GetMapping("/v1/orders")
+    //	@ResponseBody
+    @Deprecated
+    public ResponseResult<List<OrderEntity>> showAllOrders(Authentication auth) throws ParameterException {
 
-		ResponseResult<List<OrderEntity>> response = ResponseResult.getInstance(ResponseResult.STATUS_OK,
-				ResponseResult.MESSAGE_OK);
+        ResponseResult<List<OrderEntity>> response = ResponseResult.getInstance(ResponseResult.STATUS_OK,
+                ResponseResult.MESSAGE_OK);
 
-		response.setData(orderService.getAllOrders(AuthenticationUtil.getUsernameInAuthentication(auth),
-				AuthenticationUtil.getUserRoleNameInAuthentication(auth)));
+        response.setData(orderService.getAllOrders(AuthenticationUtil.getUsernameInAuthentication(auth),
+                AuthenticationUtil.getUserRoleNameInAuthentication(auth)));
 
-		return response;
-	}
+        return response;
+    }
 
-	@Operation(description = "Obtain all orders under the current identity.")
-	@ApiResponses({
-			@ApiResponse(responseCode = "200",
-					description = "All orders under current identity got successfully."),
-			@ApiResponse(responseCode = "401",
-					description = "You are not authorized to get all orders.",
-					content = {@Content(schema = @Schema(hidden = true)) }),
-	})
-	@GetMapping
-	@ResponseBody
-	public ResponseResult<PageInfo<OrderEntity>> showAllOrders(Authentication auth,
-			 @ParameterObject
-			 @PageableDefault(sort = "orderNumber", direction = Sort.Direction.DESC, size = Integer.MAX_VALUE)
-					 			Pageable pageable)
-								throws ParameterException, GlobalPreferencesMoreThanOneException,
-									GlobalPreferencesNotFoundException, DemoBugsIntroduceFailedException {
+    @Operation(description = "Obtain all orders under the current identity.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",
+                    description = "All orders under current identity got successfully."),
+            @ApiResponse(responseCode = "401",
+                    description = "You are not authorized to get all orders.",
+                    content = {@Content(schema = @Schema(hidden = true)) }),
+    })
+    @GetMapping
+    @ResponseBody
+    public ResponseResult<PageInfo<OrderEntity>> showAllOrders(Authentication auth,
+                                                               @ParameterObject
+                                                               @PageableDefault(sort = "orderNumber", direction = Sort.Direction.DESC, size = Integer.MAX_VALUE)
+                                                               Pageable pageable)
+            throws ParameterException, GlobalPreferencesMoreThanOneException,
+            GlobalPreferencesNotFoundException, DemoBugsIntroduceFailedException {
 
-		ResponseResult<PageInfo<OrderEntity>> response = ResponseResult.getInstance(ResponseResult.STATUS_OK,
-				ResponseResult.MESSAGE_OK);
+        ResponseResult<PageInfo<OrderEntity>> response = ResponseResult.getInstance(ResponseResult.STATUS_OK,
+                ResponseResult.MESSAGE_OK);
 
-		pageable = demoBugService.introduceBugWithReverseOrdersIfNeeded(pageable);
+        pageable = demoBugService.introduceBugWithReverseOrdersIfNeeded(pageable);
 
-		Page<OrderEntity> page = orderService.getAllOrders(AuthenticationUtil.getUsernameInAuthentication(auth),
-				AuthenticationUtil.getUserRoleNameInAuthentication(auth), pageable);
-		PageInfo<OrderEntity> pageInfo =  new PageInfo<>(page);
+        Page<OrderEntity> page = orderService.getAllOrders(AuthenticationUtil.getUsernameInAuthentication(auth),
+                AuthenticationUtil.getUserRoleNameInAuthentication(auth), pageable);
+        PageInfo<OrderEntity> pageInfo =  new PageInfo<>(page);
 
-		demoBugService.introduceBugWithIncorrectLocationForApprovedOrdersIfNeeded(pageInfo.getContent());
+        demoBugService.introduceBugWithIncorrectLocationForApprovedOrdersIfNeeded(pageInfo.getContent());
 
-		response.setData(pageInfo);
+        response.setData(pageInfo);
 
-		return response;
-	}
+        return response;
+    }
 }
