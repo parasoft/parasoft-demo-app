d5109423b561d29742bc061671fe58f8147f7f45
Roller Wang <rwang@parasoft.com>
2022-10-18 13:37:20 +0800

    Update order status after order detail is loaded.

--- src/main/resources/static/common/js/orders.js
+++ src/main/resources/static/common/js/orders.js
@@ -23,41 +23,45 @@ app.controller('orderHistoryController', function($rootScope, $http, $filter) {
 		});
 		return processedItems.substring(0,processedItems.lastIndexOf(" - "));
 	}
-	
+
 	history.orderItems = function(items){
 		var processedItems = "";
 		$.each(items,function(i,item){
 			var itemInfo = (i+1)+". "+item.name+" ("+item.quantity+") - ";
 			processedItems += itemInfo;
 		});
-		
+
 		return processedItems.substring(0,processedItems.lastIndexOf(" - "));
 	}
-	
+
 	history.showOrderDetail = {'show':false}
-	
-	history.openOrderDetail = function(index, orderStatus, orderNumber, reviewedByAPV, reviewedByPRCH){
-		if(!reviewedByPRCH){
-			$http({
-				method: 'PUT',
-				url: '/proxy/v1/orders/'+orderNumber,
-				data: {"status":orderStatus,"reviewedByPRCH":true,"reviewedByAPV":reviewedByAPV},
-				headers: { 'Content-Type': 'application/json' }
-			}).then(function(result) {
-				angular.element(".new_label" + index).css("visibility", "hidden");
-				//Update icon
-				getUnreviewedAmount($http,$rootScope,$filter);
-			}).catch(function(result) {
-				console.log(result);
-			});
-		}
-		
+
+	history.openOrderDetail = function(index, orderNumber){
 		$http({
 			method: 'GET',
 			url: '/proxy/v1/orders/'+orderNumber
 		}).then(function(result) {
 			history.order = result.data.data;
-			
+
+			if(!history.order.reviewedByPRCH){
+				$http({
+					method: 'PUT',
+					url: '/proxy/v1/orders/'+history.order.orderNumber,
+					data: {
+						"status": history.order.status,
+						"reviewedByPRCH": true,
+						"reviewedByAPV": history.order.reviewedByAPV
+					},
+					headers: { 'Content-Type': 'application/json' }
+				}).then(function(result) {
+					angular.element(".new_label" + index).css("visibility", "hidden");
+					//Update icon
+					getUnreviewedAmount($http,$rootScope,$filter);
+				}).catch(function(result) {
+					console.log(result);
+				});
+			}
+
 			var introduceIncorrectNumberBug = false;
 			$.each($rootScope.deBugs,function(i,debug){
 	            if(debug.demoBugsType === "INCORRECT_NUMBER_OF_ITEMS_IN_SUMMARY_OF_PENDING_ORDER"){
@@ -65,7 +69,7 @@ app.controller('orderHistoryController', function($rootScope, $http, $filter) {
 	                return false;
 	            }
 	        });
-			
+
             if(introduceIncorrectNumberBug && history.order.status === "SUBMITTED"){
             	history.totalItemQuantity = 0;
             }else{
@@ -73,20 +77,20 @@ app.controller('orderHistoryController', function($rootScope, $http, $filter) {
             	$.each(history.order.orderItems,function(i,item){
                     totalAmount += item.quantity;
                 });
-            	
+
             	history.totalItemQuantity = totalAmount;
             }
 		}).catch(function(result) {
 			console.log(result);
 		});
-		
+
 		history.showOrderDetail['show'] = true;
 	}
-	
+
 	history.closeOrderDetail = function(){
 		history.showOrderDetail['show'] = false;
 	}
-	
+
 	history.loadStyles = function(index){
 		return{
 			"top": 85 * index + "px",
@@ -97,7 +101,7 @@ app.controller('orderHistoryController', function($rootScope, $http, $filter) {
 	/**For pagination**/
 	// The item number for per page
 	history.pageSize = 10;
-	
+
 	// Load data for previous page
 	history.prev = function(currentPage){
 		history.selectPage(currentPage-1);
@@ -115,7 +119,7 @@ app.controller('orderHistoryController', function($rootScope, $http, $filter) {
 		if((history.totalPages !== 0 || history.totalPages !== undefined) && (page < 1 || page > history.totalPages)){
 			return ;
 		}
-		
+
 		$http({
 			method: 'GET',
 			url: '/proxy/v1/orders',
@@ -128,30 +132,30 @@ app.controller('orderHistoryController', function($rootScope, $http, $filter) {
 			if(totalOrders < 1){
 				$rootScope.emptyContentError = true;
 			}
-			
+
 			history.totalPages = Math.ceil(totalOrders / history.pageSize);
 			history.currentPage = history.totalPages === 0 ? 0 : page;
-			
+
 			var begin; //The index of the first order
 			var end; //The index of the last order
 			begin = 10 * (page - 1);
-			end = begin + 10; 
-			
+			end = begin + 10;
+
 			// Situation for the last index is more than legal range
 			if(end > totalOrders){
 				end = totalOrders;
 			}
-			
+
 			//Obtain the orders' list shown on the page
 			var ordersList = new Array();
 			for(var i = begin; i < end; i++){
 				ordersList.push(orders[i]);
 			}
-			
+
 			if(ordersList.length < 1){
 				history.emptyContentsMessage = true;
 			}
-			
+
 			// Update the new data on web
 			history.orders = ordersList;
 		}).catch(function(result) {
