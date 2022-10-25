0c333657527ab38e1cf1efe44cb9abecf18cadd0
tren <tren@parasoft.com>
2022-10-25 11:38:41 +0800

    @task PDA-1178 Update InventoryOperationRequestMessageDTO.

--- src/main/java/com/parasoft/demoapp/dto/InventoryOperationRequestMessageDTO.java
+++ src/main/java/com/parasoft/demoapp/dto/InventoryOperationRequestMessageDTO.java
@@ -1,5 +1,6 @@
 package com.parasoft.demoapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class InventoryOperationRequestMessageDTO {
     private InventoryOperation operation;
     private String orderNumber;
