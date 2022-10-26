0c319df705de4415d52560306e2b26ed042160bb
rliu-parasoft <rliu@parasoft.com>
2022-10-26 14:15:11 +0800

    @task PDA-1180 Consume response message and update order status

--- src/main/java/com/parasoft/demoapp/service/ItemInventoryMQService.java
+++ src/main/java/com/parasoft/demoapp/service/ItemInventoryMQService.java
@@ -1,6 +1,7 @@
 package com.parasoft.demoapp.service;
 
 import com.parasoft.demoapp.config.activemq.ActiveMQConfig;
+import com.parasoft.demoapp.dto.InventoryOperationRequestMessageDTO;
 import com.parasoft.demoapp.dto.InventoryOperationResultMessageDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsMessagingTemplate;
@@ -14,11 +15,19 @@ public class ItemInventoryMQService {
     @Autowired
     private JmsMessagingTemplate jmsMessagingTemplate;
 
+    public void sendToRequestQueue(InventoryOperationRequestMessageDTO message) {
+        jmsMessagingTemplate.convertAndSend(ActiveMQConfig.inventoryRequestActiveMqQueue, message);
+    }
+
     public void sendToResponseQueue(InventoryOperationResultMessageDTO message) {
         jmsMessagingTemplate.convertAndSend(ActiveMQConfig.inventoryResponseActiveMqQueue, message);
     }
 
     public void send(Destination destination, InventoryOperationResultMessageDTO message) {
+        jmsMessagingTemplate.convertAndSend(destination, message);
+    }
+
+    public void send(Destination destination, InventoryOperationRequestMessageDTO message) {
         jmsMessagingTemplate.convertAndSend(destination, message);
     }
 }
