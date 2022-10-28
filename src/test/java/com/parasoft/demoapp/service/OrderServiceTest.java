6c0876d75c115c1b355b7dd5a617a0ddf65ee376
tren <tren@parasoft.com>
2022-10-28 16:50:27 +0800

    @task PDA-1192 Fix unit test.

--- src/test/java/com/parasoft/demoapp/service/OrderServiceTest.java
+++ src/test/java/com/parasoft/demoapp/service/OrderServiceTest.java
@@ -1209,7 +1209,7 @@ public class OrderServiceTest {
         // Given
         String orderNumber = "11-234-567";
         OrderEntity order = prepareOrderWithIgnoringSubmmitedStatusHelper();
-        order.setStatus(OrderStatus.SUBMITTED);
+        order.setStatus(OrderStatus.PROCESSED);
         order.setOrderNumber(orderNumber);
         order.setReviewedByPRCH(true);
         order.setReviewedByAPV(false);
