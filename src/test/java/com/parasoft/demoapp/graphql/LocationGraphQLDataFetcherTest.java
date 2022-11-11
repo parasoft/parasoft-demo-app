67855333c4f736668fb2064eb94eb3b9ab14df0b
Alfie Wang <awang@parasoft.com>
2022-11-11 13:13:17 +0800

    @task PDA-1220 Add unit test for getLocation with incorrect username or password.

--- src/test/java/com/parasoft/demoapp/graphql/LocationGraphQLDataFetcherTest.java
+++ src/test/java/com/parasoft/demoapp/graphql/LocationGraphQLDataFetcherTest.java
@@ -116,4 +116,24 @@ public class LocationGraphQLDataFetcherTest {
                 .and()
                 .assertThatField(LOCATION_DATA_JSON_PATH).isNull();
     }
+
+    @Test
+    public void test_getLocation_withIncorrectUsernameOrPassword() throws IOException {
+        ObjectNode variables = objectMapper.createObjectNode();
+        variables.put("region", LOCATION_1.name());
+        GraphQLResponse response = graphQLTestTemplate
+                .withBasicAuth("incorrectUsername", "incorrectPassword")
+                .perform(LOCATION_GRAPHQL_RESOURCE, variables);
+        assertThat(response).isNotNull();
+        log.info(response.getRawResponse().getBody());
+        assertThat(response.isOk()).isTrue();
+        response.assertThatErrorsField().isNotNull()
+                .asListOf(GraphQLTestError.class)
+                .hasOnlyOneElementSatisfying(error -> {
+                    assertThat(error.getMessage()).isEqualTo(GraphQLTestErrorType.UNAUTHORIZED.toString());
+                    assertThat(error.getExtensions().get("statusCode")).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+                })
+                .and()
+                .assertThatField(LOCATION_DATA_JSON_PATH).isNull();
+    }
 }
