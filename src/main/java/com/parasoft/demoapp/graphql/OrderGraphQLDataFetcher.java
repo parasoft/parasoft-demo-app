b4656dd26010670a19fc7c5812dccf8309cc3838
tren <tren@parasoft.com>
2022-11-15 10:01:09 +0800

    @task PDA-1219&PDA-1254 Optimized code

--- src/main/java/com/parasoft/demoapp/graphql/OrderGraphQLDataFetcher.java
+++ src/main/java/com/parasoft/demoapp/graphql/OrderGraphQLDataFetcher.java
@@ -16,6 +16,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+import static com.parasoft.demoapp.service.GlobalPreferencesDefaultSettingsService.HOST;
+
 @Component
 public class OrderGraphQLDataFetcher {
 
@@ -29,7 +31,7 @@ public class OrderGraphQLDataFetcher {
 
         this.restTemplate = restTemplate;
         this.httpRequest = httpRequest;
-        this.orderBaseUrl = "http://localhost:" + webConfig.getServerPort() +"/v1/orders";
+        this.orderBaseUrl = HOST + webConfig.getServerPort() +"/v1/orders";
     }
 
     public DataFetcher<OrderEntity> getOrderByOrderNumber() {
