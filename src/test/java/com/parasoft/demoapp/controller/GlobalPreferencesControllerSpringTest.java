226e60c3369ef7936f7d3f443c232b735f444bdf
rliu-parasoft <rliu@parasoft.com>
2022-12-06 11:41:57 +0800

    Add @DirtiesContext annotation to make sure each test is new data

--- src/test/java/com/parasoft/demoapp/controller/GlobalPreferencesControllerSpringTest.java
+++ src/test/java/com/parasoft/demoapp/controller/GlobalPreferencesControllerSpringTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -29,6 +31,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext
+@TestPropertySource("file:./src/test/java/com/parasoft/demoapp/application.properties")
 public class GlobalPreferencesControllerSpringTest {
     @Autowired
     private GlobalPreferencesService globalPreferencesService;
