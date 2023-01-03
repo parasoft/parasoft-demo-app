51f40fc6535ecf015e8237a2cf13e6ad8a66aac9
rliu-parasoft <rliu@parasoft.com>
2023-01-03 11:12:46 +0800

    @task PDA-1408 Added basic error handling, and removed unnecessary code.

--- src/main/java/com/parasoft/demoapp/grpc/service/JsonServiceImpl.java
+++ src/main/java/com/parasoft/demoapp/grpc/service/JsonServiceImpl.java
@@ -1,12 +1,16 @@
 package com.parasoft.demoapp.grpc.service;
 
 
+import com.parasoft.demoapp.exception.InventoryNotFoundException;
 import com.parasoft.demoapp.exception.ParameterException;
+import com.parasoft.demoapp.messages.AssetMessages;
 import com.parasoft.demoapp.service.ItemInventoryService;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.MessageFormat;
 
 @GrpcService
 public class JsonServiceImpl extends JsonServiceImplBase {
@@ -17,14 +21,26 @@ public class JsonServiceImpl extends JsonServiceImplBase {
     public void getStockByItemId(Long itemId, StreamObserver<Integer> responseObserver) {
         try {
             Integer inStock = itemInventoryService.getInStockByItemId(itemId);
+            if (inStock == null) {
+                throw new InventoryNotFoundException(MessageFormat.format(AssetMessages.INVENTORY_NOT_FOUND_WITH_ITEM_ID, itemId));
+            }
             responseObserver.onNext(inStock);
             responseObserver.onCompleted();
         } catch (ParameterException e) {
-            responseObserver.onError(Status.UNKNOWN
+            responseObserver.onError(Status.INVALID_ARGUMENT
                     .withDescription(e.getMessage())
                     .withCause(e)
                     .asRuntimeException());
-            e.printStackTrace();
+        } catch (InventoryNotFoundException e) {
+            responseObserver.onError(Status.NOT_FOUND
+                    .withDescription(e.getMessage())
+                    .withCause(e)
+                    .asRuntimeException());
+        } catch (Throwable t) {
+            responseObserver.onError(Status.INTERNAL
+                    .withDescription(t.getMessage())
+                    .withCause(t)
+                    .asRuntimeException());
         }
     }
 }
