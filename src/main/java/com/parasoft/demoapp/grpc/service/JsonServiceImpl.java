c23054e5ad7af398d7e3e1d020dfb1985cc26418
rliu-parasoft <rliu@parasoft.com>
2023-01-06 10:25:12 +0800

    Avoid using throw clause as flow control statement

--- src/main/java/com/parasoft/demoapp/grpc/service/JsonServiceImpl.java
+++ src/main/java/com/parasoft/demoapp/grpc/service/JsonServiceImpl.java
@@ -29,17 +29,18 @@ public class JsonServiceImpl extends JsonServiceImplBase {
         try {
             Integer inStock = itemInventoryService.getInStockByItemId(itemId);
             if (inStock == null) {
-                throw new InventoryNotFoundException(MessageFormat.format(AssetMessages.INVENTORY_NOT_FOUND_WITH_ITEM_ID, itemId));
+                InventoryNotFoundException inventoryNotFoundException = new InventoryNotFoundException(
+                        MessageFormat.format(AssetMessages.INVENTORY_NOT_FOUND_WITH_ITEM_ID, itemId));
+                responseObserver.onError(Status.NOT_FOUND
+                        .withDescription(inventoryNotFoundException.getMessage())
+                        .withCause(inventoryNotFoundException)
+                        .asRuntimeException());
+                return;
             }
             responseObserver.onNext(inStock);
             responseObserver.onCompleted();
         } catch (ParameterException e) {
             responseObserver.onError(Status.INVALID_ARGUMENT
-                    .withDescription(e.getMessage())
-                    .withCause(e)
-                    .asRuntimeException());
-        } catch (InventoryNotFoundException e) {
-            responseObserver.onError(Status.NOT_FOUND
                     .withDescription(e.getMessage())
                     .withCause(e)
                     .asRuntimeException());
