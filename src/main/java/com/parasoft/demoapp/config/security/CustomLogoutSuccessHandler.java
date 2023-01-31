c5ebcd6deab68b9b32c1845dcae4d850ac95caff
tren <tren@parasoft.com>
2023-01-31 11:58:20 +0800

    @task PDA-1471 Signout the session in Keycloak after logout in PDA

--- src/main/java/com/parasoft/demoapp/config/security/CustomLogoutSuccessHandler.java
+++ src/main/java/com/parasoft/demoapp/config/security/CustomLogoutSuccessHandler.java
@@ -6,18 +6,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler{
 
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
 	@Override
 	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
 			throws IOException, ServletException {
-		
-		//redirect to login page		
+
+        if (authentication instanceof OAuth2AuthenticationToken) {
+            OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler = new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+            oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/loginPage");
+            oidcLogoutSuccessHandler.onLogoutSuccess(request, response, authentication);
+            return;
+        }
+
 		response.sendRedirect("/loginPage");
 	}
 }
