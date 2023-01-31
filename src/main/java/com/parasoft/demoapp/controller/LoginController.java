bd60025f01d0b9a3b7addb1ea3a94b71b83ac80e
dtang <dtang@parasoft.com>
2023-01-31 14:56:06 +0800

    @task PDA-1477 Verify whether keycloak can be accessed before clicking keycloak to log in

--- src/main/java/com/parasoft/demoapp/controller/LoginController.java
+++ src/main/java/com/parasoft/demoapp/controller/LoginController.java
@@ -1,11 +1,14 @@
 package com.parasoft.demoapp.controller;
 
 import com.parasoft.demoapp.service.GlobalPreferencesService;
+import com.parasoft.demoapp.util.UrlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
 
 @Controller
 public class LoginController {
@@ -29,5 +32,17 @@ public class LoginController {
         }
 
         return "login";
+    }
+
+    @GetMapping("/keycloak")
+    public String validateKeycloakServerUrl() throws IOException {
+        String keycloakServerUrl = "/oauth2/authorization/keycloak";
+        try {
+            UrlUtil.validateUrl("http://localhost:8080"+ keycloakServerUrl);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error/500";
+        }
+        return "redirect:" + keycloakServerUrl;
     }
 }
