5bf9908c748ea0df68c8c8c4472b9d72ae94f854
dtang <dtang@parasoft.com>
2023-02-01 16:30:58 +0800

    @task PDA-1477 Optimize code

--- src/main/java/com/parasoft/demoapp/controller/LoginController.java
+++ src/main/java/com/parasoft/demoapp/controller/LoginController.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.io.IOException;
-
 @Controller
 public class LoginController {
 
