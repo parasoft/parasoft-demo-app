791fcadd801cc672701207095c17a0317d206b73
lli-parasoft <lli-parasoftA@Email.com>
2023-02-08 16:57:24 +0800

    @task PDA-1490 Sign out the session in Keycloak when logout swagger doc by OAuth2

--- src/main/resources/static/common/js/swagger-ui-index.js
+++ src/main/resources/static/common/js/swagger-ui-index.js
@@ -20,9 +20,58 @@ window.onload = function() {
         deepLinking : true,
         defaultModelsExpandDepth: -1,
         presets : [ SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset ],
-        plugins : [ SwaggerUIBundle.plugins.DownloadUrl ],
+        plugins : [ SwaggerUIBundle.plugins.DownloadUrl, OAuthLogoutPlugin ],
         layout : "StandaloneLayout"
     })
+
+    function OAuthLogoutPlugin() {
+
+        var lastAuthUrl = null;
+        var idToken = null;
+
+        return {
+          statePlugins: {
+            auth: {
+              wrapActions: {
+
+                authorizeOauth2: (originalAction, system) => (payload) => {
+                  originalAction(payload);
+
+                  var auth = payload.auth;
+
+                  if (auth) {
+                    lastAuthUrl = auth.schema.get('authorizationUrl');
+                  }
+
+                  if (payload.token.id_token != null) {
+                    idToken = payload.token.id_token;
+                  }
+                },
+
+                logout: (originalAction, system) => (payload) => {
+                  originalAction(payload);
+
+                  if (lastAuthUrl) {
+                    var logoutUrl = lastAuthUrl.substring(0, lastAuthUrl.lastIndexOf('/')) + '/logout';
+                    if (idToken != null) {
+                        logoutUrl += "?id_token_hint=" + idToken;
+                    }
+
+                    try {
+                        $.ajax({
+                            url: logoutUrl,
+                            type: "GET"
+                        })
+                    } finally {
+                        window.location.reload();
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
 
     window.ui = ui
 }
