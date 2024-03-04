650df273126afba12fc9e9c8de995238577d60e7
Renee Liu <rliu@parasoft.com>
2024-03-04 17:50:03 +0800

    Declared required fields in service definition

--- src/main/java/com/parasoft/demoapp/dto/ShoppingCartDTO.java
+++ src/main/java/com/parasoft/demoapp/dto/ShoppingCartDTO.java
@@ -4,13 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class ShoppingCartDTO {
-	
+
+	@NotNull
 	private Long itemId;
-	
+
+	@NotNull
 	private Integer itemQty;
 
 }
